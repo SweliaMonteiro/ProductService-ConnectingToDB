e0732c643158a07807d1192237036d32bdb14a50
Swelia Monteiro <sweliamonteiro@Swelias-Laptop.local>
2024-05-24 03:35:32 +0530

    PaginationAndSorting

--- src/test/java/com/example/ProductServiceConnectingToDbApplicationTests.java
+++ src/test/java/com/example/ProductServiceConnectingToDbApplicationTests.java
@@ -1,13 +1,14 @@
 package com.example;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 
 @SpringBootTest
 class ProductServiceConnectingToDbApplicationTests {
 
-//    @Test
-//    void contextLoads() {
-//    }
+    @Test
+    void contextLoads() {
+    }
 
 }
